b643edea964413e6fcf0fb8a55233229eb3a7f9f
T-Sharif <tasfiats@gmail.com>
2024-07-07 18:33:03 -0500

    Update adjust_results4_isadog.py

--- adjust_results4_isadog.py
+++ adjust_results4_isadog.py
@@ -68,26 +68,44 @@ def adjust_results4_isadog(results_dic, dogfile):
            None - results_dic is mutable data type so no return needed.
     """
 
-    # Used the hints file to build a framework and modified/filled in the necessary information
+    # Creates dognames dictionary for quick matching to results_dic labels from real
+    # answer & classifier's answer
     dognames_dic = dict()
-
+    
+    # Reads in dognames from file, 1 name per line & automatically closes file
     with open(dogfile, "r") as infile:
+      # Reads in dognames from first line in file
       line = infile.readline()
+      # Processes each line in file until reaching EOF (end-of-file) by 
+      # processing line and adding dognames to dognames_dic with while loop
       while line != '':
+      # Process line by striping newline from line
         line = line.strip()
+      # Adds dogname(line) to dogsnames_dic if it doesn't already exist in the
+      # dogsnames_dic dictionary
         if line not in dognames_dic:
           dognames_dic[line] = 1
+        # Reads in next line in file to be processed with while loop
+        # if this line isn't empty
         line = infile.readline()
 
+    # Add to whether pet labels & classifier labels are dogs by appending
+    # two items to end of value(List) in results_dic. 
     for file_name, value in results_dic.items():
+      # Pet Image Label IS of Dog (e.g. found in dognames_dic)
       if value[0] in dognames_dic:
+        # Classifier Label IS image of Dog appends (1, 1) because both labels are dogs
         if value[1] in dognames_dic:
           value.extend([1,1])
+        # Classifier Label IS NOT image of dog appends (1,0) because only pet label is a dog
         else:
           value.extend([1,0])
+      # Pet Image Label IS NOT a Dog image (e.g. NOT found in dognames_dic)
       else:
+        # Classifier Label IS image of Dog appends (0, 1) because only Classifier labe is a dog
         if value[1] in dognames_dic:
           value.extend([0,1])
+         # Classifier Label IS NOT image of Dog appends (0, 0) because both labels aren't dogs
         else:
           value.extend([0,0])
             
